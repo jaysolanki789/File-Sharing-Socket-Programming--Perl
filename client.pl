d83f5beac8fca98272c168f6fb674873818a2206
AdiModi <aditya.modi.in@gmail.com>
2017-11-16 12:25:55 +0530

    Menu Driven Integration
    Added functionality to send a message or local file

--- client.pl
+++ client.pl
@@ -10,21 +10,30 @@ my $socket = new IO::Socket::INET (
 die "cannot connect to the server $!\n" unless $socket;
 print "connected to the server\n";
  
- my $file1 = "file1.txt";
- 
-my $req = "";
-open(DATA, "<file1.txt") or die "Couldn't open file file.txt, $!";
-while(<DATA>){
-   $req .= "$_";
+my $file1 = "file1.txt";
+print "This is the client application"
+print "Would you like to:"
+print "1. Read file"
+print "2. Send custom message"
+my $ch = <STDIN>
+if($ch == 1){
+	print "Reading File"
+	my $req = "";
+	open(DATA, "<file1.txt") or die "Couldn't open file file.txt, $!";
+	while(<DATA>){
+ 	  $req .= "$_";
+	} 
+
+	my $size = $socket->send($req);
+	print "sent data of length $size\n";
+}
+else{
+	my $custom = <STDIN>
+	my $size = $socket->send($req);
+	print "sent data of length $size\n";	
 } 
-
-my $size = $socket->send($req);
-print "sent data of length $size\n";
-
 shutdown($socket, 1);
- 
 my $response = "";
 $socket->recv($response, 1024);
 print "received response: $response\n";
- 
 $socket->close();
